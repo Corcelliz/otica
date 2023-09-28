2d2c3b6d02674868755d26b9870d94796d369f77
Corcelliz <guilhermecorcelli@gmail.com>
2023-09-27 22:54:51 -0300

    construindo corpo da aplicação

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import './rodapeStyle.css';
+
+export default function Rodape() {
+    return (
+        <footer>
+            <div>
+            <p>© 2022 Óticas Vida. - Todos os direitos reservados.</p>
+            </div>
+        </footer>
+    );
+}
